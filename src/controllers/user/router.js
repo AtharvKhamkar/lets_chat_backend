a80811922f8c432cad7311a7cdd1d2ffff6d730e
atharvstemmone <atharv.stemmone@gmail.com>
2025-02-17 00:12:55 +0530

    Fix case-sensitive filename issue

--- src/controllers/user/router.js
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-import controller from "./user.controller.js";
-
-export default express
-  .Router()
-  .get("/users-list", controller.getUserList)
-  .get("/detail", controller.getUserDetails);
